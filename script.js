53bdf0378ab0796d9835d2baa219f4130aa15ef7
“rafaeldiniz22” <Rafael.diniz1022@gmail.com>
2024-09-16 23:33:16 -0300

    JS bug fix, insert date.now() to image always load on button click switcher

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
 function toggleMode() {
-  const html = document.documentElement
-  html.classList.toggle("light")
+  const html = document.documentElement;
+  html.classList.toggle("light");
 
-  //Método If e Else
+  const img = document.querySelector("#profile img");
 
-  //Pegar a tag img
-  const img = document.querySelector("#profile img")
-
-  //substituir a imagem
   if (html.classList.contains("light")) {
-    //Se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/Desktop/avatar.png")
+    img.setAttribute("src", "./assets/Desktop/avatar.png?v=" + Date.now()); // Adiciona um timestamp à URL
     img.setAttribute(
       "alt",
       "Foto de Rafael Diniz de Perfil usando óculos e camiseta cinza, além de boné e fundo escuro."
-    )
+    );
   } else {
-    //Se não tiver light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/Desktop/avatar-light.png")
+    img.setAttribute("src", "./assets/Desktop/avatar-light.png?v=" + Date.now()); // Adiciona um timestamp à URL
     img.setAttribute(
       "alt",
       "Foto de Rafael Diniz de Perfil usando óculos e camiseta cinza, com as duas mãos apontando para a cabeça e fundo branco."
-    )
+    );
   }
 }
